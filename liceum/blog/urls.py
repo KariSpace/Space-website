e10ba7f64713883a542384d859457558cc8fcd09
KariSpace <52755167+KariSpace@users.noreply.github.com>
2019-12-02 00:17:56 +0200

    Add files via upload

---
+++ liceum/blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='blog-index'),
+    path('sollarSystem/', views.sollarSystem, name='blog-sollarSystem'),
+    path('history/', views.history, name='blog-history'),
+    path('blog/', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
